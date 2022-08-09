7adbf3e75d5102c7fa95e5e7a1265d67ff0f325d
Momchil Petrov <momchildp@gmail.com>
2022-08-09 15:16:32 +0200

    Added further regular expressions

--- templater.py
+++ templater.py
@@ -1,5 +1,5 @@
-debugLogFailure = "/home/smoothex/Desktop/debug.log"
-debugLogNormal = "/home/smoothex/.ccm/test_python_normal/node2/logs/debug.log"
+debugLogFailure = "/home/smoothex/Desktop/test_python_failure/debug.log"
+debugLogNormal = "/home/smoothex/Desktop/test_python_normal/debug.log"
 
 import re
 
@@ -30,14 +30,48 @@ with open(debugLogFailure, 'r+') as fp:
     # CFMetaData@5a18cd76
     cfmetadata_regex = r'CFMetaData\@.*\[.*\]'
 
-    # tokens [-107910199436536903, -1213662480926735592, -1221822647174710395,...,]
-    tokens_regex = r'tokens \[.*\]'
+    # token(s) [-107910199436536903, -1213662480926735592, -1221822647174710395,...,]
+    tokens_regex = r'token(s?) \[.*\]'
 
-    # peers@1448674088(...)
-    peers_regex = r'peers\@.*\('
+    # @1448674088(...)
+    num_regex = r'\@[0-9]+\('
 
     # Node configuration:[...]
     nodeConfiguration_regex = r'Node configuration\:\[.*\]'
+
+    # remote=9353c531-906f-300a-af1f-a3d469583b70
+    remoteServer_regex = r'remote=[a-z0-9]+\-[a-z0-9]+\-[a-z0-9]+\-[a-z0-9]+\-[a-z0-9]+'
+
+    # init = 2555904(2496K) used = 4727040(4616K) committed = 4784128(4672K) max = 251658240(245760K)
+    memory_regex = r'init = [0-9]+\([0-9]+K\) ' \
+                   r'used = [0-9]+\([0-9]+K\) ' \
+                   r'committed = [0-9]+\([0-9]+K\) ' \
+                   r'max = (-?)[0-9]+\((-?)[0-9]+K\)'
+
+    # segmentId=1658758350815, position=543
+    commitlog_regex = r'segmentId=[0-9]+\, position=[0-9]+'
+
+    # at 1658758105488 with
+    id_regex = r'at [0-9]+ with'
+
+    # me-1-big-Data.db
+    db_regex = r'm(e|d)-[0-9]+-big(-Data.db)?'
+
+    # Compacting (3e27ae50-0c23-11ed-8cad-4304f9841101)
+    compacting_regex = r'Compacting \([a-z0-9]+\-[a-z0-9]+\-[a-z0-9]+\-[a-z0-9]+\-[a-z0-9]+\)'
+    compacted_regex = r'Compacted \([a-z0-9]+\-[a-z0-9]+\-[a-z0-9]+\-[a-z0-9]+\-[a-z0-9]+\)'
+
+    # in 59ms
+    time_regex = r'in \d{1,2}ms'
+
+    # created 0858462d-89a5-441a-ab3e-9637e5b0109c
+    created_regex = r'created [a-z0-9]+\-[a-z0-9]+\-[a-z0-9]+\-[a-z0-9]+\-[a-z0-9]+'
+
+    # schema version: bcd1c7bf-1e18-3d33-9184-48929d14af1f
+    schemaVersion_regex = r'schema version(:?) [a-z0-9]+\-[a-z0-9]+\-[a-z0-9]+\-[a-z0-9]+\-[a-z0-9]+'
+
+    # InternalResponseStage:2
+    internalResponseStage_regex = r'InternalResponseStage:\d'
 
     log = re.sub(pattern=date_regex,
                  repl="<DATE>",
@@ -69,11 +103,44 @@ with open(debugLogFailure, 'r+') as fp:
     log = re.sub(pattern=tokens_regex,
                  repl="tokens [<TOKEN_IDs>]",
                  string=log)
-    log = re.sub(pattern=peers_regex,
-                 repl="peers@<ID>(",
+    log = re.sub(pattern=num_regex,
+                 repl="@<ID>(",
                  string=log)
     log = re.sub(pattern=nodeConfiguration_regex,
                  repl="Node configuration:[<NODE_CONF>]",
+                 string=log)
+    log = re.sub(pattern=remoteServer_regex,
+                 repl="remote=<IP>",
+                 string=log)
+    log = re.sub(pattern=memory_regex,
+                 repl="init = <NUM> used = <NUM> committed = <NUM> max = <NUM>",
+                 string=log)
+    log = re.sub(pattern=commitlog_regex,
+                 repl="segmentId=<NUM>, position=<NUM>",
+                 string=log)
+    log = re.sub(pattern=id_regex,
+                 repl="at <ID> with",
+                 string=log)
+    log = re.sub(pattern=db_regex,
+                 repl="<DB>",
+                 string=log)
+    log = re.sub(pattern=compacting_regex,
+                 repl="Compacting (<ID>)",
+                 string=log)
+    log = re.sub(pattern=compacted_regex,
+                 repl="Compacted (<ID>)",
+                 string=log)
+    log = re.sub(pattern=time_regex,
+                 repl="in <TIME>ms",
+                 string=log)
+    log = re.sub(pattern=created_regex,
+                 repl="created <IP>",
+                 string=log)
+    log = re.sub(pattern=schemaVersion_regex,
+                 repl="current schema version: <IP>",
+                 string=log)
+    log = re.sub(pattern=internalResponseStage_regex,
+                 repl="InternalResponseStage:<NUM>",
                  string=log)
     fp.seek(0)
     fp.write(log)
